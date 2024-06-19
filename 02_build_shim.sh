6cc7539ab0c316ad75684d43095d897e1d5d4068
Fabrice A. Marie <fabrice@evren.co>
2024-06-19 13:43:26 +0800

    OSUTIL-3618 #comment Use docker instead of Podman to make review easier.

--- 02_build_shim.sh
+++ 02_build_shim.sh
@@ -2,10 +2,18 @@
 set -e
 source VARS
 mkdir -p output
-podman build -t evren/shim-build:v${SHIM_VERSION} -f Containerfile-build \
+
+echo "Building shim" | tee toupload/02-shim-build.log
+docker build -t evren/shim-build:v${SHIM_VERSION} -f Dockerfile-build \
     --build-arg SHIM_VERSION=${SHIM_VERSION} \
-    --env SHIM_ARCHIVE_URL=${SHIM_ARCHIVE_URL} \
-    --env SHIM_ARCHIVE_FILE=${SHIM_ARCHIVE_FILE} \
-    --env SHIM_ARCHIVE_SHA256=${SHIM_ARCHIVE_SHA256} \
-    --volume=`pwd`/output:/output:rw 2>&1 | tee toupload/02-shim-build.log
-echo "Successfully generated shim"
+    --build-arg SHIM_ARCHIVE_URL=${SHIM_ARCHIVE_URL} \
+    --build-arg SHIM_ARCHIVE_FILE=${SHIM_ARCHIVE_FILE} \
+    --build-arg SHIM_ARCHIVE_SHA256=${SHIM_ARCHIVE_SHA256} \
+    . 2>&1 | tee -a toupload/02-shim-build.log
+
+echo -e "\nCopying generating shim RPM to output directory\n" | tee -a toupload/02-shim-build.log
+
+docker run --rm \
+    --volume=`pwd`/output:/output:rw \
+    evren/shim-build:v${SHIM_VERSION} 2>&1 | tee -a toupload/02-shim-build.log
+echo "Successfully generated shim and copied it to output directory"
--- reproduce_build.sh
+++ reproduce_build.sh
@@ -2,26 +2,32 @@
 source VARS
 BASE_IMG_SHA256_CONTENT="@@REPLACEME@@"
 echo "Clearing old base build image (if it exists)"
-podman rmi evren/shim-img:v${SHIM_VERSION}
+docker rmi evren/shim-img:v${SHIM_VERSION}
 set -e
-rm -rf work
+rm -rf work docker-shim-img-v${SHIM_VERSION}.tar docker-shim-img-v${SHIM_VERSION}.tar.bz2
 mkdir -p work/output
 echo "Getting the base build image"
-wget -q https://support-files.evren.co/shim/shim-img-v${SHIM_VERSION}.tar.bz2
+wget -q https://support-files.evren.co/shim/docker-shim-img-v${SHIM_VERSION}.tar.bz2
 echo "Verifying image integrity"
 echo "${BASE_IMG_SHA256_CONTENT}" | sha256sum -c
 echo "Decompressing image"
-bunzip2 shim-img-v${SHIM_VERSION}.tar.bz2
+bunzip2 docker-shim-img-v${SHIM_VERSION}.tar.bz2
 echo "Loading image in local registry"
-podman load --quiet -i shim-img-v${SHIM_VERSION}.tar
-echo "Cleaning up a bit"
-rm -f shim-img-v${SHIM_VERSION}.tar
-echo "Building shim"
-podman build -t evren/shim:v${SHIM_VERSION} -f Containerfile-reproduce \
-    --env SHIM_ARCHIVE_URL=${SHIM_ARCHIVE_URL} \
-    --env SHIM_ARCHIVE_FILE=${SHIM_ARCHIVE_FILE} \
-    --env SHIM_ARCHIVE_SHA256=${SHIM_ARCHIVE_SHA256} \
-    --env VERIFY_ATTESTATION_SCRIPT_URL=${VERIFY_ATTESTATION_SCRIPT_URL} \
-    --env PARSE_VERIFIED_ATTESTATION_SCRIPT_URL=${PARSE_VERIFIED_ATTESTATION_SCRIPT_URL} \
-    --volume=`pwd`/work:/work:rw 2>&1 | tee 03-build-reproduce.log
-echo "Reproduction built successfully completed. Please see build-reproduce.log"
+docker load -i docker-shim-img-v${SHIM_VERSION}.tar
+echo "Re-building shim" | tee 03-build-reproduce.log
+docker build -t evren/shim:v${SHIM_VERSION} -f Dockerfile-reproduce \
+    --build-arg SHIM_VERSION=${SHIM_VERSION} \
+    --build-arg SHIM_ARCHIVE_URL=${SHIM_ARCHIVE_URL} \
+    --build-arg SHIM_ARCHIVE_FILE=${SHIM_ARCHIVE_FILE} \
+    --build-arg SHIM_ARCHIVE_SHA256=${SHIM_ARCHIVE_SHA256} \
+    --build-arg VERIFY_ATTESTATION_SCRIPT_URL=${VERIFY_ATTESTATION_SCRIPT_URL} \
+    --build-arg PARSE_VERIFIED_ATTESTATION_SCRIPT_URL=${PARSE_VERIFIED_ATTESTATION_SCRIPT_URL} \
+    . 2>&1 | tee -a 03-build-reproduce.log
+
+echo -e "\nRunning review script\n" tee -a 03-build-reproduce.log
+
+docker run --rm \
+    --volume=`pwd`/work:/work2:rw \
+    evren/shim:v${SHIM_VERSION} 2>&1 | tee -a 03-build-reproduce.log | tee -a work/output/review.txt
+
+echo "Reproduction built successfully completed. Please see 03-build-reproduce.log"
